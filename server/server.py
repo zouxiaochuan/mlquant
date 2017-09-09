31ace1c3143e4e4adb002b5e48d9dbccb177f0ed
xiaochuan <zouxiaochuan@163.com>
2017-09-09 17:33:45 +0800

    finish test server

--- server/server.py
+++ server/server.py
@@ -6,19 +6,30 @@ import pymongo;
 from pymongo import MongoClient;
 
 
-db = MongoClient('mongodb://xxxx:xxxx@localhost:27017/')['mlquant'];
 app=Flask(__name__);
+
+def getdb():
+    return MongoClient('mongodb://xiaochuan:iphoipho0@localhost:27017/')['mlquant']['scores'];
 
 @app.route('/uploadScore',methods=['POST'])
 def uploadScore():
+    db = getdb();
     jsonstr = request.stream.read();
     inputs = json.loads(jsonstr);
+
+    for rec in inputs:
+        rec['_id'] = rec['secID']+'_'+rec['tradeDate'];
+        db.update({"_id":rec['_id']},
+                   rec,
+                   upsert=True
+        );
+        pass;
     
-    db.insert_many(inputs);
     return json.dumps({'success':True});
 
 @app.route('/queryScore',methods=['POST'])
 def queryScore():
+    db = getdb();
     qstr = request.stream.read();
     qres = list(db.find(json.loads(qstr)));
     ret = dict();
