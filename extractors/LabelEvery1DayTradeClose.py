e0b00aacacaea40384a57e44a0752b499bc0e590
xiaochuan <zouxiaochuan@163.com>
2019-09-16 15:07:55 +0800

    refresh code

--- extractors/LabelEvery1DayTradeClose.py
+++
@@ -1,7 +0,0 @@
-from BaseLabelEveryNDayTrade import BaseLabelEveryNDayTrade;
-
-class LabelEvery1DayTradeClose(BaseLabelEveryNDayTrade):
-    def __init__(self):
-        super(LabelEvery1DayTradeClose, self).__init__(nday=1,buy_price='closePrice',sell_price='closePrice');
-        pass;
-    pass;
--- extractors/LabelEvery2DayTrade.py
+++
@@ -1,7 +0,0 @@
-from BaseLabelEveryNDayTrade import BaseLabelEveryNDayTrade;
-
-class LabelEvery2DayTrade(BaseLabelEveryNDayTrade):
-    def __init__(self):
-        super(LabelEvery2DayTrade, self).__init__(nday=2,sell_price='openPrice');
-        pass;
-    pass;
