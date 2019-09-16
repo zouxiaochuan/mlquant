73a095b5cde3cffc67c72b38a4d7a9d420c89ffd
xiaochuan <zouxiaochuan@163.com>
2019-09-16 16:52:10 +0800

    update code

---
+++ utils_common.py
@@ -0,0 +1,153 @@
+import uuid
+import datetime
+import numpy as np
+from collections import deque
+
+
+def get_temp_name(prefix='temp'):
+    return prefix + '_' + str(uuid.uuid4()).replace('-', '_')
+
+
+def file2list(filename):
+    ret = []
+    with open(filename) as fin:
+        for line in fin:
+            ret.append(line.strip())
+            pass
+        pass
+    return ret
+
+
+def dt_add(dt, days):
+    return datetime.datetime.strftime(
+        datetime.datetime.strptime(dt, '%Y-%m-%d') +
+        datetime.timedelta(days=days), '%Y-%m-%d')
+
+
+def date2dt(date):
+    return datetime.datetime.strftime(date, '%Y-%m-%d')
+
+
+def dt2ms(dt):
+    date = datetime.datetime.strptime(dt, '%Y-%m-%d')
+    return int(date.timestamp() * 1000)
+
+
+def dt_diff(dt1, dt2):
+    date1 = datetime.datetime.strptime(dt1, '%Y-%m-%d')
+    date2 = datetime.datetime.strptime(dt2, '%Y-%m-%d')
+    return (date1-date2).days
+
+
+def get_current_dt():
+    return datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d')
+
+
+def min_sum_sublist(alist):
+    best = cur = 0
+    for v in alist:
+        cur = min(cur + v, 0)
+        best = min(best, cur)
+        pass
+    return best
+
+
+def max_sum_sublist(alist):
+    best = cur = 0
+    for v in alist:
+        cur = max(cur + v, 0)
+        best = max(best, cur)
+        pass
+    return best
+
+
+def slide_window_max(values, windowSize):
+    q = deque()
+
+    ret = []
+    for i in range(len(values)):
+        while len(q) > 0 and values[i] > values[q[-1]]:
+            q.pop()
+            pass
+        q.append(i)
+        if q[0] <= (i-windowSize):
+            q.popleft()
+            pass
+
+        ret.append(values[q[0]])
+        pass
+
+    return ret
+
+
+def slide_window_min(values, windowSize):
+    values = [-i for i in values]
+    ret = slide_window_max(values, windowSize)
+    ret = [-i for i in ret]
+    return ret
+
+
+def slide_window_mean(values, windowSize):
+    vsum = 0.0
+    vnum = 0
+    ret = []
+
+    for i in range(len(values)):
+        if i >= windowSize:
+            vsum -= values[i-windowSize]
+            vnum -= 1
+            pass
+        vsum += values[i]
+        vnum += 1
+
+        ret.append(vsum/vnum)
+        pass
+
+    return ret
+
+
+def sharp_ratio(returns, noRiskReturn):
+    if len(returns) <= 1:
+        return 0.0
+    std = np.std(returns)
+    return (returns[-1]-noRiskReturn*len(returns))/std
+
+
+def max_draw_down(returns):
+    xs = returns
+    i = np.argmax((np.maximum.accumulate(xs) - xs)/xs)  # end of the period
+    if i == 0:
+        return 0
+    j = np.argmax(xs[:i])  # start of period
+
+    return (returns[i]-returns[j])/returns[j]
+
+
+def max_continous(vals, check_func):
+    max_len = 0
+
+    current_len = 0
+    for v in vals:
+        if check_func(v):
+            current_len += 1
+        else:
+            current_len = 0
+            pass
+        max_len = max(current_len, max_len)
+        pass
+    return max_len
+
+
+def slide_window_max_continuous(vals, window_size, check_func):
+    ret = []
+
+    for i in range(len(vals)):
+        end = i+1
+        start = end-window_size
+        if start < 0:
+            start = 0
+            pass
+        ret.append(max_continous(vals[start:end], check_func))
+        pass
+    return ret
+    pass
---
+++ utils_tiger.py
@@ -0,0 +1,54 @@
+from typing import Type
+import tigeropen.common.consts as tiger_consts
+from tigeropen.tiger_open_config import TigerOpenClientConfig
+from tigeropen.quote.quote_client import QuoteClient
+from tigeropen.common.util.signature_utils import read_private_key
+
+import utils_common
+
+
+def get_client_config(config: dict) -> Type[TigerOpenClientConfig]:
+    client_config = TigerOpenClientConfig()
+    client_config.private_key = read_private_key(config['tiger_private_key'])
+    client_config.tiger_id = config['tiger_id']
+    client_config.account = config['tiger_account']
+    client_config.standard_account = config.get('tiger_standard_account')
+    client_config.paper_account = config['tiger_paper_account']
+    client_config.language = tiger_consts.Language.zh_CN
+    return client_config
+
+
+def get_quote_client(config: dict) -> Type[QuoteClient]:
+    return QuoteClient(get_client_config(config))
+
+
+def get_bars_minute(
+        quote_client: QuoteClient,
+        symbols: list,
+        begin_time: int,
+        end_time: int):
+
+    return quote_client.get_bars(
+        symbols=symbols,
+        period=tiger_consts.BarPeriod.ONE_MINUTE,
+        begin_time=begin_time,
+        end_time=end_time,
+        right=tiger_consts.QuoteRight.BR,
+        limit=600)
+
+
+def get_bars_minute_dt(
+        quote_client: QuoteClient,
+        symbols: list,
+        dt: str):
+    ms = utils_common.dt2ms(dt)
+    start = ms + 12 * 60 * 60 * 1000
+    end = ms + 48 * 60 * 60 * 1000
+
+    return quote_client.get_bars(
+        symbols=symbols,
+        period=tiger_consts.BarPeriod.ONE_MINUTE,
+        begin_time=start,
+        end_time=end,
+        right=tiger_consts.QuoteRight.BR,
+        limit=600)
